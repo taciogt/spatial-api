feb186c2e754e20c7a15b6b5ec08363b316be311
Tácio Tavares <taciogt@gmail.com>
2019-08-21 21:41:24 -0300

    script de deploy comentado

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 set -x
 
-docker build . --tag taciogt/spatial-api
-docker push taciogt/spatial-api
+#docker build . --tag taciogt/spatial-api
+#docker push taciogt/spatial-api
 
 CLUSTER=spatial-app
 ECS_PROFILE=spatial-app-profile
 
-ecs-cli configure --cluster $CLUSTER --region us-east-1 --default-launch-type EC2 --config-name $CLUSTER
-#
-ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY --profile-name $ECS_PROFILE
+#ecs-cli configure --cluster $CLUSTER --region us-east-1 --default-launch-type EC2 --config-name $CLUSTER
+##
+#ecs-cli configure profile --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY --profile-name $ECS_PROFILE
 #
 #ecs-cli up --force --keypair spatial-app --capability-iam --size 2 --instance-type t2.medium --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
 
@@ -23,5 +23,5 @@ ecs-cli ps --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
 
 #ecs-cli compose scale 2 --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
 
-ecs-cli compose service up --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
-ecs-cli ps --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
+#ecs-cli compose service up --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
+#ecs-cli ps --cluster-config $CLUSTER --ecs-profile $ECS_PROFILE
